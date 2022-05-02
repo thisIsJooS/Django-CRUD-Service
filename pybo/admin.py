ecde5cb2d7ef1789ccc1bad0b28985625b03a5a1
seungwooJoo <thisis.joos@gmail.com>
2022-05-02 20:54:25 +0900

    initial commit

---
+++ pybo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Question, Answer
+
+# Register your models here.
+admin.site.register(Answer)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+    
+admin.site.register(Question, QuestionAdmin)
